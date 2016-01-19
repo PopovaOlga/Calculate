fb5c9c809ad18eb9a6ce55af34cd764a2857ffbe
m18 <opopova_55@ukr.net>
2016-01-19 18:53:13 +0200

    к

--- src/test/java/Test_Calc.java
+++ src/test/java/Test_Calc.java
@@ -40,14 +40,14 @@ public class Test_Calc {
         // Method to check
         assertEquals("0 / 5 must be 0", 0, calc.division(0, 5));
     }
-        @Test
+      // * @Test
         public void testMultiplyWithNullValues() {
             // Class for testing
             Calc calc = new Calc();
 
             // Method to check
             assertEquals("0 * 5 must be 0", 0, calc.multiply(0, 5));
-        }
+        }//
     @After
     public void consolePrintAfter() {
         System.out.println("Congrats!!! ");
